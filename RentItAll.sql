ccf8f0278d15885b2cfd0e7ada84a9a873cf5416
ksbains <k1bains@ucsd.edu>
2019-11-15 17:16:29 -0800

    changed up the sql

--- RentItAll.sql
+++ RentItAll.sql
@@ -1,20 +1,18 @@
 DROP DATABASE IF EXISTS RentItAll_DB;
 CREATE DATABASE RentItAll_DB;
 
-USE greatBay_DB;
+USE RentItAll_DB;
 
 CREATE TABLE customer(
-  id INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(100) NOT NULL,
   password VARCHAR(45) NOT NULL,
   name VARCHAR(100) NOT NULL,
   address VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (username)
 );
 
 -- need to add FK for loc_address
 CREATE TABLE car(
-  id INT NOT NULL AUTO_INCREMENT,
   loc_address VARCHAR(100) NOT NULL,
   VIN VARCHAR(100) NOT NULL,
   type VARCHAR(45) NOT NULL,
@@ -31,41 +29,53 @@ CREATE TABLE car(
 
 -- need to add FK for loc_address
 CREATE TABLE rent_out(
-  id INT NOT NULL AUTO_INCREMENT,
   cu_username VARCHAR(100) NOT NULL,
   car_VIN VARCHAR(100) NOT NULL,
   price VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (cu_username, car_VIN)
+  FOREIGN KEY (car_VIN) references from car.VIN
+  FOREIGN KEY (cu_username) references from customer.username
+);
+
+CREATE TABLE rent(
+  cu_username VARCHAR(100) NOT NULL,
+  car_VIN VARCHAR(100) NOT NULL,
+  price VARCHAR(100) NOT NULL,
+  PRIMARY KEY (cu_username, car_VIN)
+  FOREIGN KEY (car_VIN) references from car.VIN
+  FOREIGN KEY (cu_username) references from customer.username
 );
 
 -- need to add FK for loc_address
 CREATE TABLE buy(
-  id INT NOT NULL AUTO_INCREMENT,
   cu_username VARCHAR(100) NOT NULL,
   car_VIN VARCHAR(100) NOT NULL,
   price VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (cu_username, car_VIN)
+  FOREIGN KEY (car_VIN) references from car.VIN
+  FOREIGN KEY (cu_username) references from customer.username
 );
 
 -- need to add FK for loc_address
 CREATE TABLE sell(
-  id INT NOT NULL AUTO_INCREMENT,
   cu_username VARCHAR(100) NOT NULL,
   car_VIN VARCHAR(100) NOT NULL,
   price VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (cu_username, car_VIN)
+  FOREIGN KEY (car_VIN) references from car.VIN
+  FOREIGN KEY (cu_username) references from customer.username
 );
 
 -- need to add FK for loc_address
 CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
   SSN INT NOT NULL,
   name VARCHAR(100) NOT NULL,
   loc_address VARCHAR(100) NOT NULL,
   salary INT NOT NULL,
   manager_ssn INT,
   position VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (SSN)
+  FOREIGN KEY (loc_address) references from company_locations.address
 );
 
 
@@ -81,14 +91,17 @@ CREATE TABLE maintenance_service(
   price INT NOT NULL,
   me_ssn INT NOT NULL,
   PRIMARY KEY (name)
+  FOREIGN KEY (me_ssn) references from employee.SSN
 );
 
 CREATE TABLE service_instance(
-  id INT NOT NULL AUTO_INCREMENT,
   cu_name VARCHAR(100) NOT NULL,
   price INT NOT NULL,
   car_VIN VARCHAR(100) NOT NULL,
   me_ssn INT NOT NULL,
   PRIMARY KEY (id)
+  FOREIGN KEY (cu_name) references from customer.username
+  FOREIGN KEY (me_ssn) references from employee.SSN
+  FOREIGN KEY (car_VIN) references from car.VIN  
 );
 

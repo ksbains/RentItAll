c4e4d1bff1c2d4683e01953c6fb5f2bb75d0559c
tarnjit458 <tarnjitishere@gmail.com>
2019-11-24 18:25:11 -0500

    Upadated locations to locations in san jose

--- RentItAll.sql
+++ RentItAll.sql
@@ -207,13 +207,13 @@ CREATE TABLE assist(
 );
 
 INSERT INTO customer
-VALUES  ('testuser1', '123', 'tester1', '012013012', '123 Virginia St'),
-        ('testuser2', '345', 'tester2', '012013123', '124 Virginia St'),
-        ('testuser3', '678', 'tester3', '012013345', '125 Virginia St'),
-        ('testuser4', '912', 'tester4', '012013234', '126 Virginia St'),
-        ('testuser5', '949', 'tester5', '012013233', '132 Irvington St'),
-        ('testuser6', '962', 'tester6', '012013232', '133 Irvington St'),
-        ('testuser7', '989', 'tester7', '012013231', '134 Irvington St');
+VALUES  ('testuser1', '123', 'tester1', '012013012', '315 E San Fernando'),
+        ('testuser2', '345', 'tester2', '012013123', '316 E San Fernando'),
+        ('testuser3', '678', 'tester3', '012013345', '317 E San Fernando'),
+        ('testuser4', '912', 'tester4', '012013234', '318 E San Fernando'),
+        ('testuser5', '949', 'tester5', '012013233', '168 E Taylor St'),
+        ('testuser6', '962', 'tester6', '012013232', '169 E Taylor St'),
+        ('testuser7', '989', 'tester7', '012013231', '198 Curtner Ave');
 
 INSERT INTO customer_type
 VALUES  ('testuser1', 'buyer'),
@@ -227,17 +227,17 @@ VALUES  ('testuser1', 'buyer'),
         ('testuser7', 'host');
 
 INSERT INTO company_locations
-VALUES  ('123 Washington Sq', '100', '1234567894'),
-        ('123 Irvington St', '50', '1234567498'),
-        ('123 Redwood Dr', '35', '1234567468');
+VALUES  ('315 E San Fernando', '100', '1234567894'),
+        ('189 Curtner Ave', '50', '1234567498'),
+        ('167 E Taylor St', '35', '1234567468');
 
 INSERT INTO employee
-VALUES  ('138', 'mgr0', '123 Washington Sq', '99999999'),
-        ('123456789', 'mgr1', '123 Washington Sq', '200000'),
-        ('123456788', 'mch1', '123 Washington Sq', '100000'),
-        ('123456787', 'rep1', '123 Redwood Dr', '100000'),
-        ('123456786', 'rep2', '123 Irvington St', '100000'),
-        ('123456785', 'mgr2', '123 Irvington St', '200000');
+VALUES  ('138', 'mgr0', '315 E San Fernando', '99999999'),
+        ('123456789', 'mgr1', '315 E San Fernando', '200000'),
+        ('123456788', 'mch1', '315 E San Fernando', '100000'),
+        ('123456787', 'rep1', '167 E Taylor St', '100000'),
+        ('123456786', 'rep2', '189 Curtner Ave', '100000'),
+        ('123456785', 'mgr2', '189 Curtner Ave', '200000');
 
 INSERT INTO manager
 VALUES  ('138'),  
@@ -247,47 +247,47 @@ VALUES  ('138'),
         ('123456785');
 
 INSERT INTO car
-VALUES  ('DHB100ZASFG','123 Washington Sq', NULL, 0, 'RENT', 'GAS',
+VALUES  ('DHB100ZASFG','315 E San Fernando', NULL, 0, 'RENT', 'GAS',
           'HONDA', 'ACCORD', 'GREEN','CVT-10','10000','500000','USED','2019', "yes"),
         
-        ('EF3456ZDSF4','123 Washington Sq', NULL, 0, 'RENT', 'GAS',
+        ('EF3456ZDSF4','315 E San Fernando', NULL, 0, 'RENT', 'GAS',
           'HONDA', 'CIVIC', 'PINK','CVT-10','19000','500000','USED','2001', "yes"),
         
-        ('CDE45SZQSJK','123 Washington Sq', NULL, 0, 'RENT', 'GAS',
+        ('CDE45SZQSJK','315 E San Fernando', NULL, 0, 'RENT', 'GAS',
           'HONDA', 'CIVIC', 'RED','CVT-10','18000','500000','USED','2010', "yes"),
         
-        ('FF3586DDBNM','123 Redwood Dr', NULL, 0, 'RENT', 'GAS',
+        ('FF3586DDBNM','167 E Taylor St', NULL, 0, 'RENT', 'GAS',
           'TOYOTA', 'CAMRY', 'BLUE','CVT-10','15000','500000','USED','2003', "yes"),
         
-        ('NMEFGJGKRWP','123 Washington Sq', NULL, 0, 'RENT', 'GAS',
+        ('NMEFGJGKRWP','315 E San Fernando', NULL, 0, 'RENT', 'GAS',
           'NISSIAN', 'SENTRA', 'BLACK','AUTOMATIC','13000','500000','USED','2012', "yes"),
         
-        ('IMYRN6G4RW5','123 Redwood Dr', NULL, 0, 'BOTH', 'GAS',
+        ('IMYRN6G4RW5','167 E Taylor St', NULL, 0, 'BOTH', 'GAS',
           'NISSIAN', 'SENTRA', 'YELLOW','AUTOMATIC','15000','500000','USED','2013', "yes"),
         
-        ('WRTGV335DWK','123 Washington Sq', NULL, 0, 'BOTH', 'GAS',
+        ('WRTGV335DWK','315 E San Fernando', NULL, 0, 'BOTH', 'GAS',
           'MAZDA', 'RX7', 'BLACK','MANUAL','55000','500000','USED','2000', "yes"),
         
-        ('W3T5VBJ6FWS','123 Irvington St', NULL, 0, 'BOTH', 'GAS',
+        ('W3T5VBJ6FWS','189 Curtner Ave', NULL, 0, 'BOTH', 'GAS',
           'MAZDA', 'RX7', 'WHITE','MANUAL','65000','500000','USED','2000', "yes"),
         
-        ('3FGDJKL6IPX','123 Irvington St', NULL, 0, 'BOTH', 'GAS',
+        ('3FGDJKL6IPX','189 Curtner Ave', NULL, 0, 'BOTH', 'GAS',
           'FORD', 'MUSTANG', 'BLACK','MANUAL','70000','500000','USED','2003', "yes"),
         
-        ('8DNJHSI7JDQ','123 Irvington St', NULL, 0, 'BOTH', 'GAS',
+        ('8DNJHSI7JDQ','189 Curtner Ave', NULL, 0, 'BOTH', 'GAS',
           'MAZDA', 'MX5', 'BLACK','MANUAL','30000','500000','USED','2004', "yes"),
         
-        ('2DBJDAIBCZX','123 Redwood Dr', NULL, 0, 'SELL', 'GAS',
+        ('2DBJDAIBCZX','167 E Taylor St', NULL, 0, 'SELL', 'GAS',
           'TOYOTA', 'SUPRA', 'BLACK','MANUAL','80000','500000','USED','2019', "yes"),
         
-        ('6SF53AA3C8B','123 Irvington St', NULL, 0, 'SELL', 'GAS',
+        ('6SF53AA3C8B','189 Curtner Ave', NULL, 0, 'SELL', 'GAS',
           'SUBARU', 'BRZ', 'BLACK','MANUAL','30000','500000','USED','2017', "yes"),
         
-        ('S3GHKJDF789','123 Irvington St', NULL, 0, 'SELL', 'GAS',
+        ('S3GHKJDF789','189 Curtner Ave', NULL, 0, 'SELL', 'GAS',
           'SUBARU', 'BRZ', 'GREY','MANUAL','30000','500000','USED','2013', "yes"),
         
-        ('729JSDHB42F','123 Irvington St', NULL, 0, 'SELL', 'GAS',
+        ('729JSDHB42F','189 Curtner Ave', NULL, 0, 'SELL', 'GAS',
           'TOYOTA', 'FRS', 'BLACK','MANUAL','50000','500000','USED','2011', "yes"),
         
-        ('23DSFSDG5SD','123 Irvington St', NULL, 0, 'SELL', 'GAS',
+        ('23DSFSDG5SD','189 Curtner Ave', NULL, 0, 'SELL', 'GAS',
           'NISSIAN', 'VERSA', 'BLUE','AUTOMATIC','21000','500000','USED','2008', "yes");
